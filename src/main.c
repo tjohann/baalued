f99882a9ead1e7175248084e4b0b98884cd04d0a
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2016-07-20 16:59:45 +0200

    add some more skeleton content

--- src/main.c
+++ src/main.c
@@ -20,14 +20,80 @@
 
 #include "libservice.h"
 
-int main(void)
+
+static char *program_name;
+static int running_mode = RUN_AS_APPLICATION;
+
+static void
+__attribute__((noreturn)) usage(int status)
+{
+	putchar('\n');
+	fprintf(stdout, _("Usage: %s [options]              \n"), program_name);
+	fprintf(stdout, _("Options:                                       \n"));
+
+	fprintf(stdout, _("        -h                 \
+show help                                             \n"));
+	fprintf(stdout, _("        -a                 \
+run as application in foreground                      \n"));
+	fprintf(stdout, _("        -s                 \
+run as service/daemon in background (syslog enabled)  \n"));
+	putchar('\n');
+
+	exit(status);
+}
+
+static void
+cleanup(void)
+{
+	fprintf(stdout, _("Finalize cleanup -> cheers %s\n"), getenv("USER"));
+}
+
+
+int
+main(int argc, char *argv[])
 {
 	setlocale(LC_ALL, "");
 	bindtextdomain(PACKAGE, LOCALEDIR);
 	textdomain(PACKAGE);
 
+	baa_set_program_name(&program_name, argv[0]);
 
-	printf(_("\nautotool-demo says hello\n"));
+	int c;
+	while ((c = getopt(argc, argv, "hasf:d:")) != -1) {
+		switch (c) {
+		case 'a':
+			running_mode = RUN_AS_APPLICATION;
+			break;
+		case 's':
+                        running_mode = RUN_AS_DAEMON;
+			break;
+		case 'h':
+			usage(EXIT_SUCCESS);
+			break;
+		default:
+			fprintf(stderr, _("ERROR: no valid argument\n"));
+			usage(EXIT_FAILURE);
+		}
+	}
 
-	exit (EXIT_SUCCESS);
+	if (atexit(cleanup) != 0)
+		exit(EXIT_FAILURE);
+
+	if (running_mode == RUN_AS_DAEMON) {
+		if (baa_become_daemon() != 0) {
+			baa_error_exit(_("become_daemon() != 0"));
+		} else {
+			baa_info_msg(_("now i'm a daemon"));
+		}
+	} else {
+		baa_info_msg(_("run in foreground as application"));
+	}
+
+
+
+
+	// only for testing
+	sleep(20);
+
+	return EXIT_SUCCESS;
 }
